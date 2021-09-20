c199476423bacafecf71917183dd0422fef2a9d0
Olivier <78956081+Letsgetloud@users.noreply.github.com>
2021-09-20 18:12:39 +0200

    Delete streamlit directory

--- streamlit/home.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st 
-import os                      
-import inspect                
-
-def app():
-
-    currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) 
-    st.image(os.path.join(currentdir,  '_assets_/images/Mushroom_py-cture_recognition.png'))
